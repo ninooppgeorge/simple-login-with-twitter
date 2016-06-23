0e984e4a2aa9ea7bd2fa7b3b55906751c4d53622
ninooppgeorge <work.ninooppgeorge@gmail.com>
2016-06-24 02:57:44 +0530

    added fonts ,stylesheets and font-awesome icons
    Completed the signin page styles

---
+++ signin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Simple Login with Twitter</title>
+
+    <link rel="stylesheet" href="css/com.css"/>
+    <link rel="stylesheet" href="fa/css/font-awesome.css"/>
+
+  </head>
+  <body>
+
+    <div class="container">
+      <a class="signinbutton" href="#"><i class="fa fa-twitter"></i>Sign In with Twitter</a>
+    </div>
+  </body>
+</html>
