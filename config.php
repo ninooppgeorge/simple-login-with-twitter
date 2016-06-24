792b26309b783bbe7204ea692f39968a7ec461bb
ninooppgeorge <work.ninooppgeorge@gmail.com>
2016-06-24 17:26:03 +0530

    edited config

--- config.php
+++ config.php
@@ -5,8 +5,8 @@
  * A single location to store configuration.
  */
 // CONSUMER_KEY and CONSUMER_SECRET are the key.secret pair of the twitter app.
-define('CONSUMER_KEY', 'P9EcJ18bYcA0NKkU1pBxPTII4');
-define('CONSUMER_SECRET', 'ThySzXe3dqZA5sDtTmyE6SZg8iTw1B3a10aM4HJWCM4vb6ZzE1');
+define('CONSUMER_KEY', '*******************');
+define('CONSUMER_SECRET', '****************************');
 
 // Callback URL to which Twitter should redirect with oauth_token and oauth_verifier
 define('OAUTH_CALLBACK', 'http://127.0.0.1:80/git/simple-login-with-twitter/callback.php');
